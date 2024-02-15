de9b623533664959d95d2e05d2edc398dd130120
Jposeph99 <joseph.ahmed65@yahoo.com>
2024-02-15 10:38:23 -0500

    slider is working auto now

--- main.js
+++ main.js
@@ -110,6 +110,26 @@ var swiper = new Swiper(".slide-content", {
   //   },
   // },
 });
+var swiper = new Swiper(".slide-content-video", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 var swiper = new Swiper(".slide-content1", {
   slidesPerView: 2,
